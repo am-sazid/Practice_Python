7978dcbe3798e1f766852e5f9e89f7c18f7006bc
AM Sazid <141324884+am-sazid@users.noreply.github.com>
2024-10-06 20:23:45 +0600

    Simple Practice

---
+++ practice.py
@@ -0,0 +1,8 @@
+dict1 = {"a": 2, "b": 4}
+dict2 = {"a": 2, "b": 6}
+print(dict1 != dict2)
+
+# counter = 0
+# while counter < 5:
+#     print("The counter is:", counter)
+#     counter = counter + 1
